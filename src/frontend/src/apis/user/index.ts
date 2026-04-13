cce368f4cc8d8a45e04eb55093a67f7a7b7b4169
amercado9 <alexamercad123@gmail.com>
2026-04-13 10:29:39 -0500

    register a new student user and login with this new student done.

--- src/frontend/src/apis/user/index.ts
+++ src/frontend/src/apis/user/index.ts
@@ -1,7 +1,9 @@
 import request from '@/utils/request'
-import type { ChangePasswordRequest, ChangePasswordResponse } from './types'
+import type { RegisterRequest, RegisterResponse, ChangePasswordRequest, ChangePasswordResponse } from './types'
 
 enum API { USERS = '/users' }
 
+export const registerUser = (payload: RegisterRequest) =>
+  request.post<any, RegisterResponse>(`${API.USERS}/register`, payload)
 export const changePassword = (payload: ChangePasswordRequest) =>
   request.patch<any, ChangePasswordResponse>(`${API.USERS}/change-password`, payload)
